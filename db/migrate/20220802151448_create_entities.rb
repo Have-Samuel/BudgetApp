a54695070595dd666efb1ee26d793cbe48a7f366
Have Samuel <samiehave9@gmail.com>
2022-08-02 18:28:34 +0300

    Changing the database

--- db/migrate/20220802151448_create_entities.rb
+++ db/migrate/20220802151448_create_entities.rb
@@ -1,9 +1,10 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.references :user, null: false, foreign_key: true
       t.string :name
       t.float :amount
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_204252) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_151448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "entities", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name"
-    t.float "amount"
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_entities_on_user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "groups", force: :cascade do |t|
+  create_table "entities", force: :cascade do |t|
     t.string "name"
-    t.string "icon"
+    t.float "amount"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_entities_on_category_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_204252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "entities", "categories"
   add_foreign_key "entities", "users"
 end
