300ab4d6317ac39fe231ee45d3e7fcc3136e2510
Have Samuel <samiehave9@gmail.com>
2022-08-02 19:42:54 +0300

    Created the views/categories new and the index files

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,7 @@
 class Entity < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  validates :name, presence: { message: "Name can't be blank" }
+  validates :amount, numericality: { message: "Amount can't be nothing" }
+  validates :amount, numericality: { only_float: true, greater_than: 0, message: "Amount must be greater than 0" }
 end
