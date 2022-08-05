beac52c342858df5d99d3c9ef1354f07ac075b20
Have Samuel <samiehave9@gmail.com>
2022-08-05 12:56:08 +0300

    Deploying with Heroku

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class User::SessionsController < Devise::SesionsController
+# class User::SessionsController < Devise::SesionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -12,12 +12,12 @@ class User::SessionsController < Devise::SesionsController
   # end
 
   # Delete /resource/sign_out
-  def destroy
-    sign_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if sign_out
-    yeild if block_given?
-    redirect_to root_path
-  end
+  # def destroy
+  #   sign_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+  #   set_flash_message! :notice, :signed_out if sign_out
+  #   yeild if block_given?
+  #   redirect_to root_path
+  # end
 
   # protected
 
@@ -25,4 +25,4 @@ class User::SessionsController < Devise::SesionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-end
+# end
